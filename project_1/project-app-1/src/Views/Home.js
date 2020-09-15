409ecf6e8bee9f314f8cd7fbc61dd126fa09c862
ptran12 <phitran.722@gmail.com>
2020-09-14 23:15:26 -0500

    more changes with product list

--- project_1/project-app-1/src/Views/Home.js
+++ project_1/project-app-1/src/Views/Home.js
@@ -1,14 +1,70 @@
-import React from 'react'
-import HomeWorld from '../Components/HelloWorld'
-import HelloWorld from '../Components/HelloWorld'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import Loader from '../Components/Loader'
+import ProductCard from '../Components/ProductCard'
 
 
 function Home(){
+
+    const url = `https://5f5fc91990cf8d001655726e.mockapi.io/Products?page=1&limit=10`
+    const [products, setProducts] = useState({
+        loading: false,
+        data: null
+    })
+
+    useEffect(() => {
+        setProducts({
+            loading: true,
+            data: null,
+            error: false
+        })
+        axios.get(url)
+            .then(response =>{
+                setProducts({
+                    loading: false,
+                    data: response.data,
+                    error: false
+                })
+            })
+            .catch(error => {
+                setProducts({
+                    loading: false,
+                    data: null,
+                    error: true
+                })
+            })
+    }, [url])
+
+    let content = null
+
+    if(products.error){
+        content = <p>
+            There was an error please refresh or try again later.
+        </p>
+    }
+
+    if(products.loading){
+        content = <Loader></Loader>
+    }
+
+    if(products.data){
+        content = 
+        products.data.map((product) =>
+            <div key={product.id}>
+                <ProductCard
+                    product={product}
+                />
+            </div>
+        )
+    }
+
+
     return (
         <div>
-            <h1 className="font-bold text-2xl">This is the home page</h1>
-            
-            <HelloWorld name="Phi"/>
+            <h1 className="font-bold text-2xl">
+                Best Sellers
+            </h1>
+            {content}
         </div>
     )
 }
